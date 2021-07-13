7c00e05ace4f637ef67b626485fb0269aa8866ff
Chris Philbin <chris@chrisphilbin.net>
2021-07-13 07:54:55 -0400

    Adding react client for front end

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>This is the first book</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
